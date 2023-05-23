c64cf7cd3d7635245e565dbcb1e29ff2b46efb9c
Dan Klco <klco@adobe.com>
2023-05-23 16:50:29 -0400

    Adding mocks

--- src/mocks/queue.js
+++ src/mocks/queue.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Adobe. All rights reserved.
+ * Copyright 2023 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -9,16 +9,20 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+export class MockQueueClient {
+  messages = [];
 
-export * from './blob-storage.js';
-export * as commonTypes from './common-typedefs.js';
-export * as contextHelper from './context.js'; // preserve backwards compatibility
-export { ContextHelper } from './context.js';
-export * from './fetch-retry.js';
-export * from './queue.js';
-export * from './rest-error.js';
-export * from './router.js';
-export * from './schema-validator.js';
-export * from './secret.js';
-export { CloudSearchIndexStorage } from './cloud-search-index-storage.js';
-export * as mocks from './mocks/index.js';
+  async sendMessage(message) {
+    const id = this.messages.push(message);
+    return id.toString();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async removeMessage() {
+    // do nothing
+  }
+
+  reset() {
+    this.messages = [];
+  }
+}
