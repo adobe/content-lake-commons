988e4b479c3b09968a1adec6a021fdfabf0166d1
Matt Ryan <maryan@adobe.com>
2023-05-23 16:07:26 -0600

    Fix schema validator test

--- test/schema-validator.test.js
+++ test/schema-validator.test.js
@@ -12,7 +12,7 @@
 
 /* eslint-env mocha */
 import assert from 'assert';
-import { SchemaValidator } from '../src/schema-validator.js';
+import { SCHEMA_INGESTION_REQUEST, SchemaValidator } from '../src/schema-validator.js';
 
 const SMALL_REQ = {
   data: {
@@ -36,37 +36,40 @@ describe('Schema Validator Tests', () => {
   const validator = new SchemaValidator();
   describe('validateIngestionRequest', () => {
     it('validates full object', async () => {
-      await validator.validateIngestionRequest({
-        data: {
-          sourceAssetId: '2D155092-F458-4DDC-A00C-3B003C55EF54',
-          sourceId: 'site.sharepoint.com:/sites/ASite',
-          sourceType: 'microsoft',
-          sourceUrl:
-            'https://site.sharepoint.com/sites/ASite/_layouts/15/Doc.aspx?sourcedoc=someid\u0026file=somefile.png\u0026action=default\u0026mobileredirect=true',
-          name: 'somefile.png',
-          size: 36699,
-          created: '2017-03-06T22:41:31Z',
-          createdBy: 'testuser@adobe.com',
-          lastModified: '2017-03-24T05:59:37Z',
-          lastModifiedBy: 'testuser2@adobe.com',
-          path: '/sites/ASite/somepath/somefile.png',
+      await validator.validateRequest(
+        {
+          data: {
+            sourceAssetId: '2D155092-F458-4DDC-A00C-3B003C55EF54',
+            sourceId: 'site.sharepoint.com:/sites/ASite',
+            sourceType: 'microsoft',
+            sourceUrl:
+              'https://site.sharepoint.com/sites/ASite/_layouts/15/Doc.aspx?sourcedoc=someid\u0026file=somefile.png\u0026action=default\u0026mobileredirect=true',
+            name: 'somefile.png',
+            size: 36699,
+            created: '2017-03-06T22:41:31Z',
+            createdBy: 'testuser@adobe.com',
+            lastModified: '2017-03-24T05:59:37Z',
+            lastModifiedBy: 'testuser2@adobe.com',
+            path: '/sites/ASite/somepath/somefile.png',
+          },
+          sourceMetadata: {
+            title: 'My Cool file title!',
+          },
+          binary: {
+            url: 'https://site.sharepoint.com/sites/ASite/_layouts/15/download.aspx?UniqueId=SOMEID\u0026Translate=false\u0026tempauth=SOMEJWT',
+          },
+          jobId: '123',
+          batchId: '2023-03-15T16:23:22.149Z',
+          requestId: '4556',
+          companyId: 'test',
+          spaceId: 'test',
         },
-        sourceMetadata: {
-          title: 'My Cool file title!',
-        },
-        binary: {
-          url: 'https://site.sharepoint.com/sites/ASite/_layouts/15/download.aspx?UniqueId=SOMEID\u0026Translate=false\u0026tempauth=SOMEJWT',
-        },
-        jobId: '123',
-        batchId: '2023-03-15T16:23:22.149Z',
-        requestId: '4556',
-        companyId: 'test',
-        spaceId: 'test',
-      });
+        SCHEMA_INGESTION_REQUEST,
+      );
     });
 
     it('validates limited object', async () => {
-      await validator.validateIngestionRequest(SMALL_REQ);
+      await validator.validateRequest(SMALL_REQ, SCHEMA_INGESTION_REQUEST);
     });
 
     it('fails on unexpected keys', async () => {
